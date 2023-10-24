876eab1da00535e662bf1abebd7592fe83b90f6b
Shane Monck <shane@constructivesoftware.com.au>
2023-10-24 15:39:40 +0800

    Infrastructure - Publishing Domain Events inside the unit of work

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,21 +1,66 @@
 using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
 public sealed class ApplicationDbContext: DbContext, IUnitOfWork
 {
+    #region Fields
+
+    private readonly IPublisher _publisher;
+
+    #endregion
+
     #region Construction
 
-    public ApplicationDbContext(DbContextOptions options): base(options)
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher): base(options)
     {
+        _publisher = publisher;
     }
+
+    #endregion
+
+    #region Private Methods
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
-        
+
         base.OnModelCreating(modelBuilder);
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+                           .Entries<Entity>()
+                           .Select(entry => entry.Entity)
+                           .SelectMany(entity =>
+                                       {
+                                           var domainEvents = entity.GetDomainEvents();
+
+                                           entity.ClearDomainEvents();
+                                           return domainEvents;
+                                       })
+                           .ToList();
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        // not robust
+        await PublishDomainEventsAsync();
+
+        return result;
     }
 
     #endregion
